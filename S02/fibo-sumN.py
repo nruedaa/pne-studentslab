40def7a0e816a385d85231d260895b758e0cec04
nruedaa <n.rueda.2023@alumnos.urjc.es>
2024-02-05 16:23:32 +0100

    This is going to be my first but not last commit

--- S02/fibo-sumN.py
+++ S02/fibo-sumN.py
@@ -1,10 +1,29 @@
-n_terms = int(input("Number of terms:"))
-x1 = 0
-x2 = 1
-while n_terms > 0:
-    print(x1, end=" ")
-    x3 = x1 + x2
-    x1 = x2
-    x2 = x3
+def fibonacci(n):
+    terms_list = [0]
+    variable1 = 0
+    variable2 = 1
+    while n > 0:
+        variable3 = variable1 + variable2
+        variable1 = variable2
+        variable2 = variable3
+        terms_list.append(variable1)
+        n = n - 1
+    return terms_list
 
-    n_terms = n_terms - 1
+def fibosum(n , terms_list):
+    summatory = 0
+    count = 0
+    for i in terms_list:
+        if count <= n:
+            summatory += i
+            count += 1
+
+    return summatory
+n = int(input("Number of terms:"))
+terms_list = fibonacci(n)
+summatory = fibosum(n, terms_list)
+print("Sum of the First 5 terms of the Fibonacci series:", summatory)
+n = int(input("Number of terms:"))
+terms_list = fibonacci(n)
+summatory = fibosum(n, terms_list)
+print("Sum of the First 10 terms of the Fibonacci series:", summatory)
