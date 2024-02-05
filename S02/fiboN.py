50668f8757ffae87370e55efb94193e6b403f61a
nruedaa <n.rueda.2023@alumnos.urjc.es>
2024-02-05 15:50:16 +0100

    This is going to be my first but not last commit

--- S02/fiboN.py
+++ S02/fiboN.py
@@ -1,10 +1,14 @@
-n_terms = int(input("Number of terms:"))
-x1 = 0
-x2 = 1
-while n_terms > 0:
-    print(x1, end=" ")
-    x3 = x1 + x2
-    x1 = x2
-    x2 = x3
 
-    n_terms = n_terms - 1
+terms = int(input("Number of terms:"))
+terms_list = [0]
+variable1 = 0
+variable2 = 1
+while terms > 0:
+    variable3 = variable1 + variable2
+    variable1 = variable2
+    variable2 = variable3
+    terms_list.append(variable1)
+    terms = terms - 1
+print("5th fibonacci term: ", terms_list[5])
+print("10th fibonacci term: ", terms_list[10])
+print("15th fibonacci term: ", terms_list[15])
