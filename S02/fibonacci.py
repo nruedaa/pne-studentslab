3d1b082e45c0d4622c2014c15147181f3251210d
nruedaa <n.rueda.2023@alumnos.urjc.es>
2024-02-05 15:34:14 +0100

    This is going to be my first but not last commit

--- S02/fibonacci.py
+++ S02/fibonacci.py
@@ -1,10 +1,10 @@
-n_terms = int(input("Number of terms:"))
-x1 = 0
-x2 = 1
-while n_terms > 0:
-    print(x1, end=" ")
-    x3 = x1 + x2
-    x1 = x2
-    x2 = x3
+terms = 0
+variable1 = 0
+variable2 = 1
+while terms < 11:
+    print(variable1, end=" ")
+    variable3 = variable1 + variable2
+    variable1 = variable2
+    variable2 = variable3
+    terms += 1
 
-    n_terms = n_terms - 1
