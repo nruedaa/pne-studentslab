142294a8dce6047be15e379d4f89504dfb169a83
nruedaa <n.rueda.2023@alumnos.urjc.es>
2024-02-28 18:43:22 +0100

    This is going to be my first but not last commit

--- P02/Client0.py
+++ P02/Client0.py
@@ -7,4 +7,23 @@ class Client:
     def ping(self):
         print("OK!")
     def __str__(self):
-        print("Connection to SERVER at", self.IP, "PORT:", self.PORT)
+        return "Connection to SERVER at " + self.IP + " PORT: " + str(self.PORT)
+    def talk(self, msg):
+        # -- Create the socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # establish the connection to the Server (IP, PORT)
+        s.connect((self.IP, self.PORT))
+
+        # Send data.
+        s.send(str.encode(msg))
+
+        # Receive data
+        response = s.recv(2048).decode("utf-8")
+
+        # Close the socket
+        s.close()
+
+        # Return the response
+        return response
+
--- P02/e3.py
+++ P02/e3.py
@@ -1,7 +1,7 @@
 from Client0 import Client
 
 PRACTICE = 2
-EXERCISE = 2
+EXERCISE = 3
 
 print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 
@@ -12,3 +12,6 @@ PORT = 8081
 # -- Create a client object
 c = Client(IP, PORT)
 print(c)
+print("Sending a message to the server...")
+response = c.talk("Testing!!")
+print(f"Response: {response}")
