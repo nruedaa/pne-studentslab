16fbc200d382b120199bfd804e5c81b5aeaa93fa
nruedaa <n.rueda.2023@alumnos.urjc.es>
2024-03-06 18:39:58 +0100

    commit

--- P03/SeqServer.py
+++ P03/SeqServer.py
@@ -1,37 +1,38 @@
 import socket
 import termcolor
 class Server:
-    PORT = 8080
-    IP = "127.0.0.1"
-    MAX_OPEN_REQUESTS = 5
-    number_con = 0
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        serversocket.bind((IP, PORT))
-        serversocket.listen(MAX_OPEN_REQUESTS)
-        while True:
-            print("Waiting for connections at {}, {} ".format(IP, PORT))
-            (clientsocket, address) = serversocket.accept()
-            number_con += 1
-            print("CONNECTION: {}. From the IP: {}".format(number_con, address))
-            msg = clientsocket.recv(2048).decode("utf-8")
-            print("Message from client: {}".format(msg))
-            message = "Hello from the teacher's server\n"
-            send_bytes = str.encode(message)
-            clientsocket.send(send_bytes)
-            clientsocket.close()
+    def __init__(self):
+        PORT = 8082
+        IP = "127.0.0.1"
+        MAX_OPEN_REQUESTS = 5
+        number_con = 0
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            serversocket.bind((IP, PORT))
+            serversocket.listen(MAX_OPEN_REQUESTS)
+            print("SEQ server configured")
+            while True:
+                print("Waiting for clients...")
+                (clientsocket, address) = serversocket.accept()
+                number_con += 1
+                msg = clientsocket.recv(2048).decode("utf-8")
+                message = self.execute_response(str(msg)) + "\n"
+                send_bytes = str.encode(message)
+                clientsocket.send(send_bytes)
+                clientsocket.close()
 
-    except socket.error:
-        print("Problems using ip {} port {}. Is the IP correct? Do you have port permission?".format(IP, PORT))
+        except socket.error:
+            print("Problems using ip {} port {}. Is the IP correct? Do you have port permission?".format(IP, PORT))
 
-    except KeyboardInterrupt:
-        print("Server stopped by the user")
-        serversocket.close()
+        except KeyboardInterrupt:
+            print("Server stopped by the user")
+            serversocket.close()
 
     def execute_response(self, msg):
         if msg.startswith("PING"):
-            return self.ping_command(self, msg)
-    def ping_command(self, msg):
+            return self.ping_response()
+    def ping_response(self):
         termcolor.cprint("PING command", "green")
-        response = "OK!\n"
-        print(f"{response}")
+        print("OK!")
+        return "OK\n"
+c = Server()
