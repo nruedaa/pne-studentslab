a4ff6af16ca69fbfc2ae7c0e0df73e2a4ae98074
nruedaa <n.rueda.2023@alumnos.urjc.es>
2024-03-11 11:45:13 +0100

    commit

--- P03/SeqServer.py
+++ P03/SeqServer.py
@@ -3,7 +3,7 @@ import termcolor
 from Seq1 import Seq
 class Server:
     def __init__(self):
-        PORT = 8080
+        PORT = 8082
         IP = "127.0.0.1"
         MAX_OPEN_REQUESTS = 5
         number_con = 5
@@ -24,61 +24,74 @@ class Server:
                 exit()
             else:
                 msg = clientsocket.recv(2048).decode("utf-8")
-                message = self.execute_response(str(msg)) + "\n"
-                response = str.encode(message)
+                message = self.execute_response(msg)
+                response = message.encode()
                 clientsocket.send(response)
                 clientsocket.close()
 
-    def execute_response(self, msg, n = None, sequence = None, gene = None):
+    def execute_response(self, msg):
         if msg.startswith("PING"):
             return self.ping_response()
         elif msg.startswith("GET"):
-            return self.get_response(n)
+            return self.get_response(msg)
         elif msg.startswith("INFO"):
-            return self.info_response(sequence)
+            return self.info_response(msg)
         elif msg.startswith("COMP"):
-            return self.complement_response(sequence)
+            return self.complement_response(msg)
         elif msg.startswith("REV"):
-            return self.reverse_response(sequence)
+            return self.reverse_response(msg)
         elif msg.startswith("GENE"):
-            return self.gene_response(gene)
+            return self.gene_response(msg)
+        else:
+            return "Invalid command"
     def ping_response(self):
         termcolor.cprint("PING command", "green")
         print("OK!")
         return "OK\n"
 
-    def get_response(self, n):
+    def get_response(self, msg):
+        n = msg.replace("GET", "")
         termcolor.cprint("GET", "green")
         list_sequences = ["AAAACCCCGGGGTTTT", "CCCCGGGGTTTTAAAA", "ACGTACGTACGTACGTACGTACGT", "GTGTGTCACACAGTGTGTCACA", "AAACAAATAAAGGGGCGGGTGGGA"]
-        user_sequence = list_sequences[n]
+        user_sequence = list_sequences[int(n)]
         print(user_sequence)
         return user_sequence
 
-    def info_response(self, sequence):
+    def info_response(self, msg):
         termcolor.cprint("INFO", "green")
-        s = Seq(sequence)
+        s = Seq(msg)
         bases_list = ["A", "C", "T", "G"]
         print(" Sequence:", s, "\n", "Total Length:", s.len())
         for j in bases_list:
-            average = (round(sequence.count(j) / s.len() * 100, 2))
+            average = (round(msg.count(j) / s.len() * 100, 2))
             print(j, ":", s.seq_count_base(j), "(", average, "% )")
         return " Sequence:", s, "\n", "Total Length:", s.len()
 
-    def complement_response(self, sequence):
+    def complement_response(self, msg):
+        sequence = ""
+        msg = msg.lstrip("COMP")
+        for i in msg:
+            if i in "ACGT":
+                sequence += i
         termcolor.cprint("COMP", "green")
         s = Seq(sequence)
         comp_seq = s.seq_complement()
         print(comp_seq)
         return comp_seq
 
-    def reverse_response(self, sequence):
+    def reverse_response(self, msg):
+        sequence = ""
+        for i in msg:
+            if i in "ACGT":
+                sequence += i
         termcolor.cprint("REV", "green")
         s = Seq(sequence)
         rev_seq = s.seq_reverse()
         print(rev_seq)
         return rev_seq
 
-    def gene_response(self, gene):
+    def gene_response(self, msg):
+        gene = msg.strip("GENE ")
         termcolor.cprint("GENE", "green")
         filename = "../sequences/" + gene + ".txt"
         s = Seq()
