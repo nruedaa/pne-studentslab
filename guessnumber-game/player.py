562885ac7104150d44c26bd0790daa14718d61ab
nruedaa <n.rueda.2023@alumnos.urjc.es>
2024-03-12 16:20:28 +0100

    commit

--- guessnumber-game/player.py
+++ guessnumber-game/player.py
@@ -2,8 +2,11 @@ from Client0 import Client
 IP = "127.0.0.1"  # your IP address
 PORT = 8080
 c = Client(IP, PORT)
-while True:
+correct = True
+while correct:
     number = input("Enter number")
     print(f"* Number: {number} ...")
     response = c.talk(number)
     print(f"Game says: {response}")
+    if response.startswith("You won"):
+        correct = False
