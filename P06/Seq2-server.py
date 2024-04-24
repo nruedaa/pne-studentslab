7bee44ac82152f608192ddf2a6ddad0d2674c7cc
nruedaa <n.rueda.2023@alumnos.urjc.es>
2024-04-24 18:36:08 +0200

    commit

--- P06/Seq2-server.py
+++ P06/Seq2-server.py
@@ -5,32 +5,49 @@ from pathlib import Path
 from urllib.parse import parse_qs, urlparse
 import jinja2 as j
 
-# Define the Server's port
 PORT = 8080
 def read_html_file(filename):
     contents = Path("html/" + filename).read_text()
     contents = j.Template(contents)
     return contents
+def get_response(n):
+    list_sequences = ["AAAACCCCGGGGTTTT", "CCCCGGGGTTTTAAAA", "ACGTACGTACGTACGTACGTACGT", "GTGTGTCACACAGTGTGTCACA", "AAACAAATAAAGGGGCGGGTGGGA"]
+    user_sequence = list_sequences[int(n)]
+    print(user_sequence + "\n")
+    return user_sequence + "\n"
+def seq_read_fasta(filename):
+    from pathlib import Path
+    first_line = Path(filename).read_text().find("\n")
+    body = Path(filename).read_text()[first_line:]
+    body = body.replace("\n", "")
+    return body
 
-# -- This is for preventing the error: "Port already in use"
+sequence_1 = seq_read_fasta("../sequences/U5.txt")
+sequence_2 = seq_read_fasta("../sequences/ADA.txt")
+sequence_3 = seq_read_fasta("../sequences/FRAT1.txt")
+sequence_4 = seq_read_fasta("../sequences/FXN.txt")
+
 socketserver.TCPServer.allow_reuse_address = True
 
-
-# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
-# It means that our class inherits all his methods and properties
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
+        url_path = urlparse(self.path)
+        path = url_path.path
+        arguments = parse_qs(url_path.query)
         termcolor.cprint(self.requestline, 'green')
-        if self.path == "/" or self.path.startswith("/echo"):
+        if path == "/" or path.startswith("/echo"):
             contents = Path('./html/index.html').read_text()
-            self.send_response(200)  # -- Status line: OK!
-            url_path = urlparse(self.path)
-            path = url_path.path
-            arguments = parse_qs(url_path.query)
-            if path == "/ping":
-                contents = Path('./html/ping.html').read_text()
-                self.send_response(200)
+            self.send_response(200)
+        elif path == "/ping":
+            contents = Path('./html/ping.html').read_text()
+            self.send_response(200)
+        elif path == "/get":
+            number = arguments.get("sequence", [""])[0]
+            sequence = get_response(number)
+            contents = read_html_file("get.html").render(
+                context={"number": number, "sequence": sequence})
+            self.send_response(200)
         else:
             contents = Path('./html/error.html').read_text()
             # Generating the response message
