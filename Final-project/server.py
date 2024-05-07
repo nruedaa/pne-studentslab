306f1f52ce1eb4c2a158bb3de5aed45e33ad0f4d
nruedaa <n.rueda.2023@alumnos.urjc.es>
2024-05-07 15:30:21 +0200

    trying from home

---
+++ Final-project/server.py
@@ -0,0 +1,58 @@
+import http.server
+import socketserver
+import termcolor
+from pathlib import Path
+from urllib.parse import parse_qs, urlparse
+import jinja2 as j
+
+PORT = 8080
+socketserver.TCPServer.allow_reuse_address = True
+class TestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        url_path = urlparse(self.path)
+        path = url_path.path
+        arguments = parse_qs(url_path.query)
+        termcolor.cprint(self.requestline, 'green')
+        if path == "/" or path.startswith("/echo"):
+            contents = Path('./Final-project/index.html').read_text()
+            self.send_response(200)
+        elif path == "/ping":
+            pass
+        else:
+            contents = Path('./Final-project/error.html').read_text()
+            # Generating the response message
+            self.send_response(404)
+
+        # Define the content-type header:
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(contents)))
+
+        # The header is finished
+        self.end_headers()
+        print("intro seq:", contents)
+
+        # Send the response message
+        self.wfile.write(str.encode(contents))
+
+        return
+
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stopped by the user")
+        httpd.server_close()
