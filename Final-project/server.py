337767827cbedb04ac0f02db669a33ffdd5917e9
nruedaa <n.rueda.2023@alumnos.urjc.es>
2024-05-17 01:42:59 +0200

    trying from home

--- Final-project/server.py
+++ Final-project/server.py
@@ -117,10 +117,8 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 except ConnectionRefusedError:
                     print("ERROR! Cannot connect to the Server")
                     exit()
-
                 r1 = conn.getresponse()
                 print(f"Response received!: {r1.status} {r1.reason}\n")
-
                 data1 = r1.read().decode("utf-8")
                 response = json.loads(data1)
                 seq = response["seq"]
@@ -132,13 +130,106 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 # Generating the response message
                 self.send_response(404)
         elif path == "/geneInfo":
-            pass
+            gene_name = (arguments.get("gene", [""])[0]).upper()
+            SERVER = "rest.ensembl.org"
+            ENDPOINT = f"/lookup/symbol/human/{gene_name}"
+            PARAMS = "?content-type=application/json"
+            conn = http.client.HTTPConnection(SERVER)
+            try:
+                conn.request("GET", ENDPOINT + PARAMS)
+            except ConnectionRefusedError:
+                print("ERROR! Cannot connect to the Server")
+                exit()
+            r1 = conn.getresponse()
+            print(f"Response received!: {r1.status} {r1.reason}\n")
+            data1 = r1.read().decode("utf-8")
+            response = json.loads(data1)
+            gene_id = response["id"]
+            start_point = response["start"]
+            end_point = response["end"]
+            chromosome_name = response["seq_region_name"]
+            gene_length = int(end_point) - int(start_point)
+            contents = read_html_file("information.html").render(
+                context={"gene": gene_name, "gene_id": gene_id, "start": start_point, "end": end_point, "chromosome": chromosome_name, "gene_length": gene_length})
+            self.send_response(200)
+        elif path == "/geneCalc":
+            gene_name = (arguments.get("gene", [""])[0]).upper()
+            SERVER = "rest.ensembl.org"
+            ENDPOINT = "/sequence/id/"
+            id = get_gene_id(gene_name)
+            PARAMS = "?content-type=application/json"
+            conn = http.client.HTTPConnection(SERVER)
+            if id:
+                try:
+                    conn.request("GET", ENDPOINT + id + PARAMS)
+                except ConnectionRefusedError:
+                    print("ERROR! Cannot connect to the Server")
+                    exit()
+                r1 = conn.getresponse()
+                print(f"Response received!: {r1.status} {r1.reason}\n")
+                data1 = r1.read().decode("utf-8")
+                response = json.loads(data1)
+                seq = response["seq"]
+                sequence_length = len(seq)
+                a_count = round(seq.count("A")/sequence_length, 2)
+                c_count = round(seq.count("C")/sequence_length, 2)
+                g_count = round(seq.count("G")/sequence_length, 2)
+                t_count = round(seq.count("T")/sequence_length, 2)
+                contents = read_html_file("bases_average.html").render(
+                    context={"gene": gene_name, "seq_length": sequence_length, "A": a_count, "C": c_count, "G": g_count, "T": t_count})
+                self.send_response(200)
+            else:
+                contents = Path('./html/error.html').read_text()
+                # Generating the response message
+                self.send_response(404)
+        elif path == "/geneList":
+            species_name = (arguments.get("species", [""])[0]).upper()
+            chromosome_name = (arguments.get("chromo", [""])[0]).upper()
+            start_point = (arguments.get("start", [""])[0])
+            end_point = (arguments.get("end", [""])[0])
+            SERVER = "rest.ensembl.org"
+            ENDPOINT = f"/overlap/region/{species_name}/{chromosome_name}:{start_point}-{end_point}"
+            PARAMS = "?feature=gene&content-type=application/json"
+            conn = http.client.HTTPConnection(SERVER)
+            try:
+                conn.request("GET", ENDPOINT + PARAMS)
+            except ConnectionRefusedError:
+                print("ERROR! Cannot connect to the Server")
+                exit()
+            r1 = conn.getresponse()
+            print(f"Response received!: {r1.status} {r1.reason}\n")
+            data1 = r1.read().decode("utf-8")
+            response = json.loads(data1)
+            gene_id_list = []
+            for i in response:
+                gene_id_list.append(i["gene_id"])
+            gene_name_list = []
+            for j in gene_id_list:
+                endpoint_find_gene = f"/lookup/id/{j}"
+                parameters = "?content-type=application/json"
+                conn2 = http.client.HTTPConnection(SERVER)
+                try:
+                    conn2.request("GET", endpoint_find_gene + parameters)
+                except ConnectionRefusedError:
+                    print("ERROR! Cannot connect to the Server")
+                    exit()
+                r2 = conn2.getresponse()
+                print(f"Response received!: {r2.status} {r2.reason}\n")
+                data2 = r2.read().decode("utf-8")
+                response2 = json.loads(data2)
+                if "display_name" in response2:
+                    gene_name_list.append(response2["display_name"])
+                else:
+                    if response2["description"]:
+                        gene_name_list.append(response2["description"])
+            contents = read_html_file("genes_list.html").render(
+                context={"species": species_name, "chromosome_name": chromosome_name, "start": start_point, "end":end_point, "list_genes": gene_name_list})
+            self.send_response(200)
         else:
             contents = Path('./html/error.html').read_text()
             # Generating the response message
             self.send_response(404)
 
-        # Define the content-type header:
         self.send_header('Content-Type', 'text/html')
         self.send_header('Content-Length', len(str.encode(contents)))
 
